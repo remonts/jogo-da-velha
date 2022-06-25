f0559db10a4e8b7982fcd477607239fd4f231732
Renan Monteiro <monteiro-renan@hotmail.com>
2022-06-25 09:53:37 -0300

    Criado modulo jogo da velha.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha/jogo-da-velha.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        JogoDaVelhaModule
       ],
       declarations: [
         AppComponent
