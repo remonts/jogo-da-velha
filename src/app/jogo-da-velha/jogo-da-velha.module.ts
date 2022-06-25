76aac0510653afe2aae05658e7dce457968149fb
Renan Monteiro <monteiro-renan@hotmail.com>
2022-06-25 15:55:49 -0300

    Criado html e css do jogo.

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,21 +1,21 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { JogoDaVelhaComponent } from './';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 import { JogoDaVelhaService } from './shared';
 
 @NgModule({
-  declarations: [
-    JogoDaVelhaComponent
-  ],
   imports: [
     CommonModule
   ],
+  declarations: [
+  	JogoDaVelhaComponent
+  ],
   exports: [
-    JogoDaVelhaComponent
+  	JogoDaVelhaComponent
   ],
   providers: [
-    JogoDaVelhaService
+  	JogoDaVelhaService
   ]
 })
 export class JogoDaVelhaModule { }
