cac4d9a84ba2cc2bf9d567d0a2dd9f546896eacb
Renan Monteiro <monteiro-renan@hotmail.com>
2022-06-25 15:16:10 -0300

    Criado serviço jogo da velha.

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-
+import { JogoDaVelhaComponent } from './';
+import { JogoDaVelhaService } from './shared';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JogoDaVelhaComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    JogoDaVelhaComponent
+  ],
+  providers: [
+    JogoDaVelhaService
   ]
 })
 export class JogoDaVelhaModule { }
